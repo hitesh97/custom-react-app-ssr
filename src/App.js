bd2e9a68f9d076816fb64b19920d14864da11497
Hitesh Khatri <hitesh.khatri@microlease.com>
2018-09-28 16:50:10 +0100

    SSR fixed and style started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
+import { Layout, Menu, Icon } from 'antd';
+
 //components
 import Emoji from './Emoji';
 
@@ -16,21 +18,49 @@ import Routes from './routes';
 
 class App extends Component {
   render() {
+    const { Header, Content, Footer, Sider } = Layout;
     return (
-      <div className="App">
-        <div className={cssStyles.header}>
+      <Layout className="App">
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
           <img src={logo} className="App-logo" alt="logo" />
-          <h2 className="App-title">
-            <Emoji label="danger" emoji="☢" />
-            <span> custom-react-scripts </span>
-            <Emoji label="danger" emoji="☢" />
-          </h2>
-          <div className="App-subtitle">
-            allow custom config for create-react-app without ejecting
-          </div>
-        </div>
-        <Routes />
-      </div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="user" />
+              <span className="nav-text">nav 4</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header>header</Header>
+          <Content className="appContent">
+            <div>
+              <Routes />
+            </div>
+          </Content>
+          <Footer>header</Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
